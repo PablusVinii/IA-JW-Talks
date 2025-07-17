866d557159d9368c1f88418edf83b147f9f879a1
Pablus Vinii <pablus.vinicius@live.com>
2025-07-17 13:45:48 -0300

    On branch main 	modified:   script.js

--- script.js
+++ script.js
@@ -141,7 +141,7 @@ function gerarEsboco() {
 }
 
 // Configurações da API
-const API_URL = 'http://localhost:5678/webhook-test/fd061969-eb2c-4355-89da-910ec299d4ef';
+const API_URL = 'https://primary-production-5b08.up.railway.app/webhook-test/fd061969-eb2c-4355-89da-910ec299d4ef';
 
 const elementos = {
     tipoDiscurso: document.getElementById('tipoDiscurso'),
